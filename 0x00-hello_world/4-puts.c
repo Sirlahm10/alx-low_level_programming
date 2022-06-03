6a96a5d31c1412affde52f711fdeb639391aa44f
Salam Ariyo <ariyosalamola@gmail.com>
2022-06-02 18:51:55 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/*
+*main - Entry point
+*This is a C program to return a constant
+*Return:- Always 0 (Success)
+*/
 int main(void)
 {
 puts("Programming is like building a multilingual puzzle\n");
-return(0);
+return (0);
 }
