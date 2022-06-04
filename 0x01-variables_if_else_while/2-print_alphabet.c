b715d0143978386a9edcacc24b07509b81942117
Salam Ariyo <ariyosalamola@gmail.com>
2022-06-04 11:07:01 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,7 +9,10 @@ int main(void)
 {
 	char alph;
 
-	for (alph = 'a'; alph <= 'z'; alph++)
-		     putchar(alph);
-	return (0);
+for (alph = 'a'; alph <= 'z'; alph++)
+{
+putchar(alph);
+putchar('\n');
+}
+return (0);
 }
