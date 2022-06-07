da894b63c80abd07c34ba663f6a0f51f50b765f1
Salam Ariyo <ariyosalamola@gmail.com>
2022-06-07 16:25:06 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+		_putchar(alph);
+
+	_putchar('\n');
+
+return (0);
+}	
