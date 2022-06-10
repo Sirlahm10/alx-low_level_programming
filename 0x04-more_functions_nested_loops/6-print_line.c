a90c586a0fb63c5e62867deb7f709d063d20655d
Salam Ariyo <ariyosalamola@gmail.com>
2022-06-10 13:36:05 -0700

    task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
-* print_line - Draws a straight line using the character _.
-* @n: The number of _ characters to be printed.
-*/
+ *  * print_line - Draws a straight line using the character _.
+ *   * @n: The number of _ characters to be printed.
+ *    */
 void print_line(int n)
 {
-	int len;
+		int len;
 
-	if (n > 0)
-	{
-		for (len = 0; len < n; len++)
-			putchar('');
-	}
-	_putchar('\n');
+			if (n > 0)
+					{
+								for (len = 0; len < n; len++)
+												putchar('');
+									}
+
+				_putchar('\n');
 }
